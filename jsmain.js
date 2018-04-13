1ffff4c18b3bd6c2fb81343727b1e06b4233463e
Taras Krehowetsky <kryhowetsky25@gmail.com>
2018-04-13 17:10:29 +0300

    first commit add all files

---
+++ jsmain.js
@@ -0,0 +1,38 @@
+
+// Haha mp3
+
+
+var beepOne = $("#ha-ha")[0];
+$("#rimshot-hover-sound")
+    .mouseenter(function() {
+        beepOne.play();
+    });
+
+
+
+var rimShotAudio = document.getElementById("ha-ha");
+
+function playRim() {
+    rimShotAudio.play();
+}
+item.addEventListener('mouseover', rimShotAudio.playRim, false);
+
+
+//Easy
+
+var easyOne = $("#easy")[0];
+$("#hover-easy")
+    .mouseenter(function() {
+        easyOne.play();
+    });
+
+
+
+
+
+var rimEasy = document.getElementById("easy");
+
+function playRim2() {
+    rimEasy.play();
+}
+item.addEventListener('mouseover', rimEasy.playRim2, false);
